aa9a633b4991e719645790dfce1b5e6ea3dc803b
Anshul Goel <anshul.goel@priceline.com>
2016-11-18 22:45:09 -0500

    test(setup): first React class

---
+++ src/task.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+let Task = React.createClass({
+  render: () => {
+    return (
+      <h1>My first page</h1>
+    );
+  }
+});
+
+// ReactDOM.render(<Task/>, document.getElementById('root'));
